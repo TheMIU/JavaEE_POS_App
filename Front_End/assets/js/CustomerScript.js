fa76a629adfd60363b09a6d2ae934b4e90585e8d
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-08-25 09:26:11 +0530

    Initial commit

---
+++ Front_End/assets/js/CustomerScript.js
@@ -0,0 +1,92 @@
+getAllCustomers();
+
+$("#btnGetAll").click(function () {
+    getAllCustomers();
+});
+
+function getAllCustomers() {
+    $("#tblCustomer").empty();
+
+    $.ajax({
+        url: 'customer',
+        dataType: "json",
+        method: "GET",
+        success: function (customers) {
+            for (let i in customers) {
+                let cus = customers[i];
+                let id = cus.id;
+                let name = cus.name;
+                let address = cus.address;
+                let row = `<tr><td>${id}</td><td>${name}</td><td>${address}</td></tr>`;
+                $("#tblCustomer").append(row);
+            }
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+}
+
+// add
+$("#btnCustomer").click(function () {
+    let formData = $("#customerForm").serialize();
+
+    $.ajax({
+        url: "customer",
+        method: "POST",
+        data: formData,
+        success: function (res) {
+            alert(res.message);
+            getAllCustomers();
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+});
+
+// delete
+$("#btnCusDelete").click(function () {
+    let id = $('#txtCustomerID').val();
+
+    $.ajax({
+        url: 'customer?cusID=' + id,
+        method: 'DELETE',
+
+        success: function (res) {
+            alert(res.message);
+            getAllCustomers();
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+});
+
+// update
+$("#btnUpdate").click(function () {
+    let id = $('#txtCustomerID').val();
+    let name = $('#txtCustomerName').val();
+    let address = $('#txtCustomerAddress').val();
+
+    let customer = {
+        "cusID": id,
+        "cusName": name,
+        "cusAddress": address
+    }
+
+    $.ajax({
+        url: 'customer',
+        method: 'PUT',
+        contentType: "application/json",
+        data: JSON.stringify(customer),
+
+        success: function (res) {
+            alert(res.message);
+            getAllCustomers();
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+});
---
+++ Front_End/assets/js/ItemScript.js
@@ -0,0 +1,95 @@
+getAllItems();
+
+$("#btnGetAllItems").click(function () {
+    getAllItems();
+});
+
+function getAllItems() {
+    $("#tblItem").empty();
+
+    $.ajax({
+        url: 'item',
+        dataType: "json",
+        method: "GET",
+        success: function (items) {
+            for (let i in items) {
+                let item = items[i];
+                let code = item.code;
+                let description = item.itemName;
+                let qtyOnHand = item.qty;
+                let unitPrice = item.unitPrice;
+                let row = `<tr><td>${code}</td><td>${description}</td><td>${qtyOnHand}</td><td>${unitPrice}</td></tr>`;
+                $("#tblItem").append(row);
+            }
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+}
+
+// add
+$("#btnItem").click(function () {
+    let formData = $("#itemForm").serialize();
+
+    $.ajax({
+        url: "item",
+        method: "POST",
+        data: formData,
+        success: function (res) {
+            alert(res.message);
+            getAllItems();
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+});
+
+// delete
+$("#btnItemDelete").click(function () {
+    let code = $('#itemCode').val();
+
+    $.ajax({
+        url: "item?code=" + code,
+        method: "DELETE",
+
+        success: function (res) {
+            alert(res.message);
+            getAllItems();
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+});
+
+// update
+$("#btnItemUpdate").click(function () {
+    let code = $('#itemCode').val();
+    let itemName = $('#itemName').val();
+    let qty = $('#itemQty').val();
+    let unitPrice = $('#itemPrice').val();
+
+    let item = {
+        "code": code,
+        "itemName": itemName,
+        "qty": qty,
+        "unitPrice": unitPrice
+    }
+
+    $.ajax({
+        url: "item",
+        method: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify(item),
+
+        success: function (res) {
+            alert(res.message);
+            getAllItems();
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
+        }
+    });
+});
